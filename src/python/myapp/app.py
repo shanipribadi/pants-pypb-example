4cf9fe31b91de2b7408550123519a379aa2533c4
Shani <shanipribadi@gmail.com>
2022-03-26 10:49:12 +0100

    pants tailor and requirements

--- src/python/myapp/app.py
+++ src/python/myapp/app.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from google.type.money_pb2 import Money
-from my.package import Message
-import text_format from google.protobuf
+from my.package.message_pb2 import Message
+from google.protobuf import text_format
 
 if __name__ == '__main__':
     m = Message(amount=Money(currency_code="USD", units=10))
